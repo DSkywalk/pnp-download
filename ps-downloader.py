1bad655713309aac95ca7fb8d380156e28806c7e
David Skywalker <dantoine@gmail.com>
2015-10-05 19:50:15 +0200

    new version now supports cowcow/artscow

--- ps-downloader.py
+++ ps-downloader.py
@@ -23,17 +23,12 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
-import sys, urllib, urllib2, cookielib
+import sys, urllib, optparse
 from BeautifulSoup import BeautifulSoup, CData
 from urlparse import urlparse, parse_qs
-import optparse
 
-USER_AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-LOGIN_URL = "https://secure.printerstudio.com/login.aspx"
-EDIT_URL = "http://www.printerstudio.com/products/pro_project_edit.aspx"
-DINAMIC_URL = "http://www.printerstudio.com/products/playingcard/design/dn_playingcards_front_dynamic.aspx?"
-FINAL_PATH = "/products/playingcard/design/dn_playingcards_front_dynamic.aspx?id="
 
+""" fix ssl v1 problems - force TLSv1 """
 from requests.adapters import HTTPAdapter
 try:
     from requests.packages.urllib3.poolmanager import PoolManager
@@ -47,77 +42,210 @@ class MyAdapter(HTTPAdapter):
                                        maxsize=maxsize,
                                        block=block,
                                        ssl_version=ssl.PROTOCOL_TLSv1)
-def download(p_oLogged, p_sURL, p_sBaseName):
-    print "Downloading images..."
-    #print p_oLogged.cookies.get_dict()
-    request = p_oLogged.get(p_sURL, headers=USER_AGENT)
-    page = BeautifulSoup(request.text)
-    imgs = page.find(id='hidd_images_info')
-    try:
-        oImgs = eval( imgs['value'] )
-    except:
-        print " "
-        print "ERROR! Can not enter in: " + p_sURL
-        print " "
-        print " Try to logged in PS and retry again..."
-        return
 
-    iCounter = 0
-    for img in oImgs:
-        sFile = img['ID'] + "." + img['Exp']
-        sUrl = img['Path'] + "/" + sFile
-        print "d: " + sFile
-        if p_sBaseName:
-            sFile = p_sBaseName + "_" + "{0:04d}".format(iCounter) + "." + img['Exp']
-            iCounter += 1
+""" virtual class for pnp sites """
+class webpnp:
+    def __init__(self, p_sUrl):
+        self.m_dHeaders = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+        self.m_sLoginUrl = ""
+        self.m_sEditUrl = ""
+        self.m_sDinamicUrl = ""
+        self.m_dPayload = {}
+        self.m_sFilenameCustom = lOptions.filename
+        self.m_sUserUrl = p_sUrl
+        self.m_needUser = False
+        self.m_netloc = urlparse(p_sUrl).netloc[4:] # allow ps.de/.es/.com ...
+        
+        import requests
+        self.m_oSession = requests.Session()
+        self.m_oSession.mount('https://', MyAdapter()) # force ssl v1
+        
+        try: # remove windows warnings
+            requests.packages.urllib3.disable_warnings()
+        except:
+            pass
             
-        urllib.urlretrieve (sUrl, sFile)
-
-
-def prepare_url(p_oLogged, p_sUrl):
-    oUrl = urlparse(p_sUrl)
-    oQuery = parse_qs(oUrl.query)
+        # finally call setup
+        self.setup()
+        
     
-    if "dn_playingcards_front_dynamic" in p_sUrl:
-        print " Warning, trying use direct url without auth, if dont work please use 'pro_project_render.aspx' LINK"
-        return p_sUrl, False
+    def log_file(self, p_sFile, p_sTxt):
+        with open(p_sFile, 'w') as file_:
+            file_.write(p_sTxt.encode('utf-8'))
+            file_.close()
 
-    if "dn_show_parse" in p_sUrl:
-        return DINAMIC_URL + "id=" + oQuery['id'][0] + "&ssid=" + oQuery['ssid'][0], False
+    def setup(self):
+        pass
+
+    def download(self, p_sUrl):
+        pass
     
-    req = p_oLogged.post(EDIT_URL, data=oQuery, headers=USER_AGENT)
-    jsval = BeautifulSoup(req.text)
-    for cdata in jsval.findAll(text=True):
-        if isinstance(cdata, CData):
-            response = eval("%r" % cdata)
-            if response[:4] == 'http':
-                return response, False
-            else:
-                print response + " ??"
-
-    print " ERROR! Can not get final url, please use pro_project_render.aspx or dn_playingcards_front_dynamic.aspx LINKS"
-    print " Also you would try to use user/pass args..."
-    return "", True
-
-def login_ps(p_oLogin, p_oPayload):
-    p_oLogin.mount('https://', MyAdapter())
-    response = p_oLogin.get(LOGIN_URL, headers=USER_AGENT, verify=False)
-    # print response.headers
-    html = BeautifulSoup(response.text)
-    state = html.find(id='__VIEWSTATE')
-    if not state['value']:
+    def prepare_url(self):
+        return "unsupported yet", True
+    
+    def login(self):
         return False
+        
 
-    p_oPayload['__VIEWSTATE'] = state['value'];
-    p_oLogin.post(LOGIN_URL, data=p_oPayload, headers=USER_AGENT)
-    return p_oLogin
+
+class PrinterStudio(webpnp):
+
+    """     setup with ps elements    """
+    def setup (self):
+        print "PS Class USED!"
+        self.m_needUser = False
+        self.m_sLoginUrl = "https://secure.%s/login.aspx"
+        self.m_sEditUrl = "http://www.%s/products/pro_project_edit.aspx"
+        self.m_sDinamicUrl = "http://www.%s/products/playingcard/design/dn_playingcards_front_dynamic.aspx?"
+        self.m_dPayload = {
+            '__EVENTTARGET' : 'btn_submit',
+            '__EVENTARGUMENT' : '',
+            'txt_email' : lOptions.username,
+            'txt_password' : lOptions.password,
+            'ckb_remember' : False,
+            'btn_submit' : 'Login',
+        }
+
+   
+    """     download images using final url    """
+    def download(self, p_sURL):
+        print "Downloading images..."
+        #print p_oLogged.cookies.get_dict()
+        request = self.m_oSession.get(p_sURL, headers=self.m_dHeaders)
+        page = BeautifulSoup(request.text)
+        imgs = page.find(id='hidd_images_info')
+        try:
+            oImgs = eval( imgs['value'] )
+        except:
+            print " "
+            print "ERROR! Can not enter in: " + p_sURL
+            print " "
+            print " Try to logged in PS and retry again..."
+            return
+
+        iCounter = 0
+        for img in oImgs:
+            sFile = img['ID'] + "." + img['Exp']
+            sUrl = img['Path'] + "/" + sFile
+            print "d: " + sFile
+            if self.m_sFilenameCustom:
+                sFile = self.m_sFilenameCustom + "_" + "{0:04d}".format(iCounter) + "." + img['Exp']
+                iCounter += 1
+                
+            urllib.urlretrieve (sUrl, sFile)
+
+
+    """     from user URL to internal URL    """
+    def prepare_url(self):
+        oUrl = urlparse(self.m_sUserUrl)
+        oQuery = parse_qs(oUrl.query)
+        
+        if "dn_playingcards_front_dynamic" in self.m_sUserUrl:
+            print " Warning, trying use direct url without auth, if dont work please use 'pro_project_render.aspx' LINK"
+            return self.m_sUserUrl, False
+
+        if "dn_show_parse" in self.m_sUserUrl:
+            return (self.m_sDinamicUrl % self.m_netloc) + "id=" + oQuery['id'][0] + "&ssid=" + oQuery['ssid'][0], False
+        
+        req = self.m_oSession.post(self.m_sEditUrl % self.m_netloc, data=oQuery, headers=self.m_dHeaders)
+        jsval = BeautifulSoup(req.text)
+        for cdata in jsval.findAll(text=True):
+            if isinstance(cdata, CData):
+                response = eval("%r" % cdata)
+                if response[:4] == 'http':
+                    return response, False
+                else:
+                    print response + " ??"
+
+        print " ERROR! Can not get final url, please use pro_project_render.aspx or dn_playingcards_front_dynamic.aspx LINKS"
+        print " Also you would try to use user/pass args..."
+        return "", True
+
+    """     login to https site and save cookies   """
+    def login(self):
+        response = self.m_oSession.get(self.m_sLoginUrl % self.m_netloc, headers=self.m_dHeaders, verify=False)
+        # print response.headers
+        html = BeautifulSoup(response.text)
+        state = html.find(id='__VIEWSTATE')
+        if not state['value']:
+            return False
+
+        self.m_dPayload['__VIEWSTATE'] = state['value']
+        self.m_oSession.post(self.m_sLoginUrl % self.m_netloc, data=self.m_dPayload, headers=self.m_dHeaders)
+        return True
+
+class XCow(webpnp):
+    def setup(self):
+        print "CW Class USED!"
+        self.m_needUser = True
+        self.m_dHeaderJson = dict(self.m_dHeaders.items() + [('content-type', 'application/json')])
+        self.m_sLoginUrl = "https://www.%s/default.aspx/Login"
+        self.m_sDinamicUrl = "http://www.%s/SlideShow.aspx?f=Load&folder="
+        self.m_sImageUrl = "http://c2.%s/img/12-" # -0-0-1
+        self.m_dPayload = {
+                'Email': lOptions.username,
+                'Password': lOptions.password,
+           }
+
+    def prepare_url(self):
+        request = self.m_oSession.get(self.m_sUserUrl, headers=self.m_dHeaders)
+        page = BeautifulSoup(request.text)
+        form = page.find('form')
+        oQuery = parse_qs(form.get('action')[17:]) # remove "FileManager.aspx?"
+        
+        if not 'folder' in oQuery:
+            print "ERROR! Folder URL not found..."
+            print "action: " + form.get('action')
+            return "", True
+          
+        return (self.m_sDinamicUrl % self.m_netloc) + oQuery['folder'][0], False
+
+    def download(self, p_sURL):
+        request = self.m_oSession.get(p_sURL, headers=self.m_dHeaders)
+        # print response.headers
+        # print response.status_code
+        # self.log_file("cowcow.txt", request.text)
+        lImages = request.text.split('\n')
+        TotalImages = len(lImages)
+        if TotalImages < 2:
+            print " "
+            print "ERROR! Images not found at " + p_sURL
+            print " "
+        
+        print " Downloading: " + str(TotalImages) + " images."
+        iCounter = 0
+        for img in lImages[1:]: # first is project info, no need it atm
+            lImgData = img.split(',')
+            # print lImgData 
+            sFile = lImgData[1]
+            sUrl = (self.m_sImageUrl % self.m_netloc) + lImgData[0] + "-0-0-1"
+            print "d: " + sFile
+            if self.m_sFilenameCustom:
+                sFile = self.m_sFilenameCustom + "_" + "{0:04d}".format(iCounter) + "." + sFile[-3:]
+                iCounter += 1
+                
+            urllib.urlretrieve (sUrl, sFile)
+
+
+    def login(self):
+        import json
+        response = self.m_oSession.post(self.m_sLoginUrl % self.m_netloc, data=json.dumps(self.m_dPayload), headers=self.m_dHeaderJson, verify=False)
+        dResut = eval(response.text)
+        if dResut['d'][0] != 'OK':
+            print dResut['d'][1]
+            return False
+        return True
+
+"""
+****  MAIN 
+"""
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage='%prog [options] <url> ',
                                version='0.1',)
     install_opts  = optparse.OptionGroup( parser, 'Download Options',
                                           'These options control downloads.', )
-                                          
+    
     install_opts.add_option('--filename', action='store', default=False,
                         help='Use as base name ex: myname_0001, myname_0002, ...')
 
@@ -128,7 +256,6 @@ if __name__ == '__main__':
     install_opts.add_option('--password', action='store', default=False,
                         help='your printerstudio password.')
     
-
     
     parser.add_option_group(install_opts)
     lOptions, lArgs = parser.parse_args()
@@ -136,35 +263,30 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit(1)
 
-    import requests
-    oSession = requests.Session()
 
-    if lOptions.username and lOptions.password:
-        # login
-        print "Login auth..."
-        try:
-            requests.packages.urllib3.disable_warnings()
-        except:
-            pass
-
-        payload = {
-            '__EVENTTARGET' : 'btn_submit',
-            '__EVENTARGUMENT' : '',
-            'txt_email' : lOptions.username,
-            'txt_password' : lOptions.password,
-            'ckb_remember' : False,
-            'btn_submit' : 'Login',
-        }
-
-        login_ps(oSession, payload)
+    oWeb = None
+    if "printerstudio" in lArgs[0]:
+        oWeb = PrinterStudio(lArgs[0])
     else:
-        print " Warning, you are not logged in PS. Some LINKS could not work ..."
+        oWeb = XCow(lArgs[0])
+
+    userLogged = False
+    if lOptions.username and lOptions.password:
+        userLogged = oWeb.login()
+
+    if not oWeb.m_needUser:
+        print " Warning, you are not logged. Some LINKS could not work ..."
+    elif not userLogged:
+        print "ERROR! This site needs a correct user/pass."
+        sys.exit(0)
+        
 
     print "Getting URL..."
-    sUrl, bError = prepare_url(oSession, lArgs[0])
-    print "Using: %r" % sUrl
+    sUrl, bError = oWeb.prepare_url()
     
     if not bError:
-        download(oSession, sUrl, lOptions.filename)
+        print "Using: %r" % sUrl
+        oWeb.download(sUrl)
         
     sys.exit(0)
+
